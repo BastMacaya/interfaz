f47c02a5edeb3c9e7aca571786f35a821deb0e35
BastMacaya <macayabastian@gmail.com>
2022-02-14 19:13:27 -0300

    hash funcional
    -Hash contraseña, faltan pruebas e integracion de jwt

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HolaMundo() {
+  return <div><h1>Hola Mundo</h1></div>;
+}
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,36 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from 'react-router-dom';
+
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+
+import { Login } from '../components/login/login';
+import { DashboardRoutes } from './DashboardRoutes';
+
+
+export const AppRouter = () => {
+
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                
+                <Route path="/login" element={ 
+                    <PublicRoute>
+                        <Login />
+                    </PublicRoute>
+                 } />
+                
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <DashboardRoutes />
+                    </PrivateRoute>
+                } />
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
